a9c0a5d38c1de719b3855c96b693ebfe9e9283af
Eki Iman Jordiansyah <ekiimanjordiansyah212@gmail.com>
2024-06-07 16:57:13 +0700

    initial

---
+++ src/pages/Homapage.jsx
@@ -0,0 +1,12 @@
+import PageNav from "../components/PageNav";
+
+function Homapage() {
+  return (
+    <div>
+      <PageNav />
+      <h1>WorldWise</h1>
+    </div>
+  );
+}
+
+export default Homapage;
