a83741e04368c3161fb2dbec3b33a81a2f7149b4
Eki Iman Jordiansyah <ekiimanjordiansyah212@gmail.com>
2024-06-08 08:20:03 +0700

    initial today

--- src/components/AppNav.jsx
+++ src/components/AppNav.jsx
@@ -1,7 +1,7 @@
-import { nav } from "./AppNav.module.css";
+import styles from "./AppNav.module.css";
 
 function AppNav() {
-  return <nav className={nav}>App navigation</nav>;
+  return <nav className={styles.nav}>App navigation</nav>;
 }
 
 export default AppNav;
