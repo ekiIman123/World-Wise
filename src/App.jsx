ab562ca5693685273c0a0ea13d9530e11d6417d3
Eki Iman Jordiansyah <ekiimanjordiansyah212@gmail.com>
2024-06-14 23:23:17 +0700

    to night

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 
 import Homepage from "./pages/Homepage";
-import Login from "./pages/Login";
 import Product from "./pages/Product";
-import Pricing from "./pages/Pricing";
 import PageNotFound from "./pages/PageNotFound";
+import Pricing from "./pages/Pricing";
+import Login from "./pages/Login";
 import AppLayout from "./pages/AppLayout";
 import CityList from "./components/CityList";
 import CountryList from "./components/CountryList";
