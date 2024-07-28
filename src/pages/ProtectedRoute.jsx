77c29cc88ec1882837bc24df180c88a42d4fe5f1
Eki Iman Jordiansyah <ekiimanjordiansyah212@gmail.com>
2024-07-28 17:13:04 +0700

    finalization

--- src/pages/ProtectedRoute.jsx
+++ src/pages/ProtectedRoute.jsx
@@ -1,17 +1,31 @@
-import { useEffect } from "react";
+// import { useEffect } from "react";
+// import { useAuth } from "../contexts/FakeAuthContext";
+// import { useNavigate } from "react-router-dom";
+
+// export default function ProtectedRoute({ children }) {
+//   const { isAuthenthicated } = useAuth();
+//   const navigate = useNavigate();
+
+//   useEffect(
+//     function () {
+//       if (isAuthenthicated) navigate("/");
+//     },
+//     [isAuthenthicated, navigate]
+//   );
+
+//   return isAuthenthicated ? children : null;
+// }
+
+import { Navigate } from "react-router-dom";
 import { useAuth } from "../contexts/FakeAuthContext";
-import { useNavigate } from "react-router-dom";
 
-export default function ProtectedRoute({ children }) {
-  const { isAuthenthicated } = useAuth();
-  const navigate = useNavigate();
+function ProtectedRoute({ children }) {
+  const { isAuthenticated } = useAuth();
 
-  useEffect(
-    function () {
-      if (isAuthenthicated) navigate("/");
-    },
-    [isAuthenthicated, navigate]
-  );
-
+  if (!isAuthenticated) {
+    return <Navigate to="/" />;
+  }
   return children;
 }
+
+export default ProtectedRoute;
